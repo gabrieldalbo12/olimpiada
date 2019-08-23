4c1387c0f3d8e1d39d08528c94a97f51964ba9d9
root <root@localhost.localdomain>
2019-08-23 09:57:55 -0300

    cuarto commit

--- api/models.py
+++ api/models.py
@@ -7,4 +7,18 @@ class Generation(models.Model):
     tension = models.IntegerField()
     corriente = models.IntegerField()
     velocidad_viento = models.IntegerField()
-    direccion_viento = models.IntegerField()
+    DIRECCION_VIENTO = [
+        ('ESTE', 'Este'),
+        ('OESTE', 'Oeste'),
+        ('NORTE', 'Norte'),
+        ('SUR', 'Sur'),
+    ]
+    direccion_viento = models.CharField(
+        max_length=10,
+        choices=DIRECCION_VIENTO,
+        default='ESTE',
+    )
+
+
+    def __str__(self):
+        return '{}, {}, {}, {}'.format(self.tension, self.corriente, self.velocidad_viento, self.direccion_viento)
